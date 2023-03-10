4c12e5b210a7171602f43ad0fb1725ccfb8db89b
Krystal <1760739491@qq.com>
2023-03-10 16:56:06 +0800

    +规范了项目结构 +项目说明文档readme -不需要的冗余文件

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template("static/views/loginView.html")
+    return render_template("loginView.html")
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login_index():
     if request.method == 'POST':
+
         print("success")
         return redirect(url_for('login'))
     else:
